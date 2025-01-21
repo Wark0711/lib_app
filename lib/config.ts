c7792ce0e9d787f30933d2df70627759e51b096c
Wark0711 <mickey.rahul98@gmail.com>
2025-01-21 16:50:24 +0530

    Added upstash redis and workflows details

--- lib/config.ts
+++ lib/config.ts
@@ -6,7 +6,13 @@ const config = {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY!
         },
-        databaseUrl: process.env.NEON_DB_URL!
+        databaseUrl: process.env.NEON_DB_URL!,
+        upstash: {
+            redisUrl: process.env.UPSTASH_REDIS_URL!,
+            redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+            qstashUrl: process.env.QSTASH_URL!,
+            qstashToken: process.env.QSTASH_TOKEN!
+        }
     }
 }
 
